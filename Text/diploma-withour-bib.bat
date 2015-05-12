ea698ee1ece7011a97e9a159f74ad7b866b0aa2c
Timofey Prodanov <prodanovtimo@gmail.com>
2015-05-12 12:55:38 +0300

    First Commit

---
+++ Text/diploma-withour-bib.bat
@@ -0,0 +1,2 @@
+xelatex diploma
+C:\Users\Timo\AppData\Local\Yandex\YandexBrowser\Application\browser.exe diploma.pdf
---
+++ Text/diploma.bat
@@ -0,0 +1,5 @@
+xelatex diploma
+bibtex diploma
+xelatex diploma
+xelatex diploma
+C:\Users\Timo\AppData\Local\Yandex\YandexBrowser\Application\browser.exe diploma.pdf
